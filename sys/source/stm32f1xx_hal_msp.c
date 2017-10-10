106c65337c1de8d2387f555d13ab36b0a8e526a3
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2017-10-10 15:23:43 +0200

    Updated clocks for USB and ADC.
    The stm32f1xx_hal_msp.c will setup the correct clocks for USB and ADC
    peripherals. These peripherals are not used but correct clocking scheme is
    needed to prevent any instabillities that may come from invalid clocking.
    
    Signed-off-by: Nenad Radulovic <nenad.b.radulovic@gmail.com>

--- sys/source/stm32f1xx_hal_msp.c
+++ sys/source/stm32f1xx_hal_msp.c
@@ -49,23 +49,26 @@ static void hw_error(void)
 /**
  * @brief       System Clock Configuration
  *              The system Clock is configured as follows:
- *              * System Clock source            = PLL (HSE)
- *              * SYSCLK(Hz)                     = 72000000
- *              * HCLK(Hz)                       = 72000000
- *              * AHB Prescaler                  = 1
- *              * APB1 Prescaler                 = 2
- *              * APB2 Prescaler                 = 1
- *              * PLLMUL                         = 9
- *              * Flash Latency(WS)              = 0
+ *              * HSE Frequency(Hz)     = 8000000
+ *              * System Clock source   = PLL (HSE)
+ *              * SYSCLK(Hz)            = 72000000
+ *              * HCLK(Hz)              = 72000000
+ *              * AHB Prescaler         = 1
+ *              * APB1 Prescaler        = 2
+ *              * APB2 Prescaler        = 1
+ *              * PLLMUL                = 9
+ *              * Flash Latency(WS)     = 2
  */
 static void setup_clock_mcu(void)
 {
     RCC_ClkInitTypeDef clkinitstruct = {0};
     RCC_OscInitTypeDef oscinitstruct = {0};
+    RCC_PeriphCLKInitTypeDef rccperiphclkinit = {0};
 
+    /* Main oscillator setup */
     oscinitstruct.OscillatorType  = RCC_OSCILLATORTYPE_HSE;
     oscinitstruct.HSEState        = RCC_HSE_ON;
-    oscinitstruct.HSEPredivValue  = RCC_HSE_PREDIV_DIV2;
+    oscinitstruct.HSEPredivValue  = RCC_HSE_PREDIV_DIV1;
     oscinitstruct.PLL.PLLState    = RCC_PLL_ON;
     oscinitstruct.PLL.PLLSource   = RCC_PLLSOURCE_HSE;
     oscinitstruct.PLL.PLLMUL      = RCC_PLL_MUL9;
@@ -73,6 +76,24 @@ static void setup_clock_mcu(void)
     if (HAL_RCC_OscConfig(&oscinitstruct) != HAL_OK) {
         hw_error();
     }
+
+    /* USB clock selection */
+    rccperiphclkinit.PeriphClockSelection = RCC_PERIPHCLK_USB;
+    rccperiphclkinit.UsbClockSelection = RCC_USBCLKSOURCE_PLL_DIV1_5;
+
+    if (HAL_RCCEx_PeriphCLKConfig(&rccperiphclkinit) != HAL_OK) {
+        hw_error();
+    }
+
+    /* ADC clock selection */
+    rccperiphclkinit.PeriphClockSelection = RCC_PERIPHCLK_ADC;
+    rccperiphclkinit.AdcClockSelection = RCC_ADCPCLK2_DIV6;
+
+    if (HAL_RCCEx_PeriphCLKConfig(&rccperiphclkinit) != HAL_OK) {
+        hw_error();
+    }
+
+    /* System clock slection */
     clkinitstruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK |
                                RCC_CLOCKTYPE_PCLK1  | RCC_CLOCKTYPE_PCLK2);
     clkinitstruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
@@ -80,10 +101,16 @@ static void setup_clock_mcu(void)
     clkinitstruct.APB2CLKDivider = RCC_HCLK_DIV1;
     clkinitstruct.APB1CLKDivider = RCC_HCLK_DIV2
 
-    if (HAL_RCC_ClockConfig(&clkinitstruct, FLASH_LATENCY_0) != HAL_OK) {
+    if (HAL_RCC_ClockConfig(&clkinitstruct, FLASH_LATENCY_2) != HAL_OK) {
         hw_error();
     }
 }
+
+static void teardown_clock_mcu()
+{
+    HAL_RCC_DeInit()
+}
+
 
 /*==========================================  GLOBAL FUNCTION DEFINITIONS  ==*/
 
@@ -95,6 +122,7 @@ void HAL_MspInit(void)
 
 void HAL_MspDeInit(void)
 {
+    teardown_clock_mcu();
 }
 
 /*===============================*//** @cond *//*==  CONFIGURATION ERRORS  ==*/
